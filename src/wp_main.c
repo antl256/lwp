79460b9720b9e52642ec912f9557c4f19c53de7a
anton <antonbranstedt@telia.com>
2026-08-11 15:06:57 +0200

    add proper arg processing in main

--- src/wp_main.c
+++ src/wp_main.c
@@ -1,5 +1,4 @@
 #include "wp_def.h"
-#include "wp_log.h"
 #include "wp_player.h"
 #include "wp_wav.h"
 
@@ -8,38 +7,72 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define FLAG_HELP    "--help"
+#define FLAG_VERSION "--version"
+
+#define checkarg(arg, str) (strncmp(arg, str, sizeof(str)) == 0)
+
 static wp_wav *s_wav = NULL;
 static wp_stream *s_stream = NULL;
 
-static void cleanup_exit(int signal) {
+static void sigint() {
+    fprintf(stdout, "\n");
+    exit(EXIT_SUCCESS);
+}
+
+static void cleanup(void) {
     wp_player_free(s_stream);
     wp_wav_free(s_wav);
-    printf("\n");
-    wp_log_info("Exiting");
-    exit(0);
+    s_stream = NULL;
+    s_wav = NULL;
+}
+
+static void help() {
+    printf("usage: %s WAVFILE VOLUME\n", WP_NAME);
+}
+
+static void version() {
+    printf("%s version %d.%d.%d\n", WP_NAME, WP_MAJOR, WP_MINOR, WP_PATCH);
 }
 
 int main(int argc, const char **argv) {
-    if (argc > 1 && strncmp(argv[1], "--version", sizeof("--version")) == 0) {
-        printf("%s version %d.%d.%d\n", WP_NAME, WP_MAJOR, WP_MINOR, WP_PATCH);
-        return 0;
+
+    atexit(cleanup);
+    signal(SIGINT, sigint);
+
+    if (argc < 2) {
+        help();
+        exit(EXIT_FAILURE);
     }
 
-    if (argc < 3) {
-        printf("usage: %s WAVFILE VOLUME\n", WP_NAME);
-        return 0;
+    for (int i = 1; i < argc; i++) {
+        const char *arg = argv[i];
+        /* Process flags. */
+        if (checkarg(arg, FLAG_HELP)) {
+            help();
+            exit(EXIT_SUCCESS);
+        } else if (checkarg(arg, FLAG_VERSION)) {
+            version();
+            exit(EXIT_SUCCESS);
+        }
     }
 
-    signal(SIGINT, &cleanup_exit);
+    const char *path = NULL;
+    const char *volume = NULL;
 
-    s_wav = wp_wav_read(argv[1]);
+    if (argv[1][0] != '\0') path = argv[1];
+    if (argv[2][0] != '\0') volume = argv[2];
+
+    if (argc < 3 || path == NULL || volume == NULL) {
+        help();
+        exit(EXIT_FAILURE);
+    }
+
+    s_wav = wp_wav_read(path);
     s_stream = wp_player_create(&s_wav);
 
-    wp_player_upload(s_stream, strtof(argv[2], NULL));
+    wp_player_upload(s_stream, strtof(volume, NULL));
     wp_player_drain(s_stream);
 
-    wp_player_free(s_stream);
-    wp_wav_free(s_wav);
-
-    return 0;
+    exit(EXIT_SUCCESS);
 }
