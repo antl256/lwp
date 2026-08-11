75c3ed7bb1ae30f45dc71a844e27d46aac6101b4
anton <antonbranstedt@telia.com>
2026-08-11 15:09:48 +0200

    change style for log messages

--- src/wp_log.h
+++ src/wp_log.h
@@ -5,8 +5,8 @@
 
 #include <stdio.h>
 
-#define wp_log_info_overwrite(format, ...) fprintf(stdout, "\033[1G\033[1K" WP_NAME ": [info] " format, ##__VA_ARGS__)
-#define wp_log_info(format, ...)           fprintf(stdout, WP_NAME ": [info] " format "\n", ##__VA_ARGS__)
+#define wp_log_info_overwrite(format, ...) fprintf(stdout, "\033[1G\033[1K" WP_NAME ": \e[34minfo:\e[0m " format, ##__VA_ARGS__)
+#define wp_log_info(format, ...)           fprintf(stdout, WP_NAME ": \e[34minfo:\e[0m " format "\n", ##__VA_ARGS__)
 #define wp_log_warning(format, ...)        fprintf(stderr, WP_NAME ": \033[33m[warning]\033[0m " format "\n", ##__VA_ARGS__)
 #define wp_log_error(format, ...)          fprintf(stderr, WP_NAME ": \033[31m[error]\033[0m " format "\n", ##__VA_ARGS__)
 
